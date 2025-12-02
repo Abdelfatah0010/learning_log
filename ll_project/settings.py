853964fe86c99253cf877852e77d0b8d3beed7f2
him <abdelfatah3939@gmail.com>
2025-12-02 12:07:39 +0200

    added my app as csrtf trusted origin

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -134,6 +134,7 @@ LOGIN_URL = 'accounts:login'
 
 
 # Railway settings
+CSRF_TRUSTED_ORIGINS = ['https://learninglog-production-965b.up.railway.app']
 ALLOWED_HOSTS = ['learninglog-production-965b.up.railway.app', '.railway.app', 'localhost', '127.0.0.1']
 import os
 DATABASES = {
